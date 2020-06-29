78ba73068369b67ec966710237a746b0b1970b89
Zákány Balázs <zakany.balazs@viapangroup.com>
2020-06-29 20:47:25 +0200

    Typo fix

--- frontend/index.js
+++ frontend/index.js
@@ -89,7 +89,7 @@ export default class __API {
       let uploadedFiles = await Promise.all(promises)
       this.setFileFields(Data, uploadedFiles)
 
-      this.$axios.$post(`/${this.Prefix}/${this._capitalize(Model)}`, JOSN.stringify(Data))
+      this.$axios.$post(`/${this.Prefix}/${this._capitalize(Model)}`, JSON.stringify(Data))
         .then( r => resolve(r))
         .catch( async Error => {
           promises = []
@@ -145,7 +145,7 @@ export default class __API {
   }
   Update( Model, Data ) {
     return new Promise((resolve, reject) => {
-      this.$axios.$patch(`/${this.Prefix}/${this._capitalize(Model)}`, JOSN.stringify(Data))
+      this.$axios.$patch(`/${this.Prefix}/${this._capitalize(Model)}`, JSON.stringify(Data))
       .then( r => resolve(r))
       .catch( Error => reject(Error))
     })
